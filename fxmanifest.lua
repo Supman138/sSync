1c79ece4e76c1431b65b871fa0f626b2fdf3c6f5
Supman138 <59351787+Supman138@users.noreply.github.com>
2021-10-25 18:29:16 -0600

    Initial Release

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+author 'Supman138'
+description 'Server Synced Time and Weather. Originally Created by Vespura (vSync)'
+version '1.0.0'
+url 'https://supman138.com'
+game 'gta5'
+fx_version 'cerulean'
+
+files { 
+    'version.json'
+}
+client_scripts {
+    'sSync_client.lua',
+}
+
+server_scripts {
+    'sSync_server.lua',
+}
